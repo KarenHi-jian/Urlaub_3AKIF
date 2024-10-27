c7d7a6a8b660d645695a2309e73e967ae1253d18
karen Hi-Jian Garcia Pinal <gar240730@spengergasse.at>
2024-10-27 16:59:37 +0100

    Aktuell

---
+++ Urlaub.java
@@ -0,0 +1,113 @@
+public class Urlaub
+{
+    private String  ort;
+    private int     preis;
+    private boolean allInclusive;
+    
+    public Urlaub(String ort, int preis, boolean allInclusive)
+    {
+        setOrt(ort);
+        setPreis(preis);
+        setAllInclusive(allInclusive);
+    }
+    
+    public Urlaub(String ort, boolean allInclusive)
+    {
+        setOrt(ort);
+        setPreis(1999);
+        setAllInclusive(allInclusive);
+    }
+    
+    public Urlaub(String ort)
+    {
+        setOrt(ort);
+        setPreis(1999);
+        setAllInclusive(false);
+    }
+    
+    public Urlaub()
+    {
+        setOrt("Wien");
+        setPreis(1999);
+        setAllInclusive(false);        
+    }
+    
+    public String getOrt()
+    {
+        return ort;
+    }
+    
+    public int getPreis()
+    {
+        return preis;
+    }
+    
+    public boolean getAllInclusive()
+    {
+        return allInclusive;
+    }
+    
+    public String getZielort()
+    {
+        int pos;
+        String zielOrt;
+        
+        pos = ort.indexOf("-");         //5
+        zielOrt = ort.substring(0, pos);
+        return zielOrt;
+    }
+    
+    public String getLand()
+    {
+        int pos;
+        String land;
+        
+        pos = ort.indexOf("-")+1;
+        land = ort.substring(pos);
+        return land;
+    }
+    
+    public void setOrt(String ort)
+    {
+        this.ort = ort;
+    }
+    
+    // 500 .. 3000
+    public void setPreis(int preis)
+    {
+        if ((preis >= 500) && (preis <= 3000))
+        {
+            this.preis = preis;
+        }
+        else
+        {
+            System.out.println("Fehler: ungültiger Preis. Muss zw. 500 und 3000 sein!");
+            this.preis = 1999;
+        }
+    }
+    
+    public void setAllInclusive(boolean allInclusive)
+    {
+        this.allInclusive = allInclusive;
+    }
+    
+    /*
+     * New York - 2310 EUR - all Inclusive
+     * Berlin - 999 EUR - keine Verpflegung
+     */
+    public void printUrlaub()
+    {
+        if (allInclusive == true)
+        {
+            System.out.println(ort + " - " + preis + " EUR - all Inclusive");
+        }
+        else
+        {
+            System.out.println(ort + " - " + preis + " EUR - keine Verpflegung");
+        }
+    }
+}
+
+
+
+
